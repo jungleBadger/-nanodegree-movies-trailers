a8fef9b92fcd2fbc5ef8ac112bee5ace57271777
Daniel Abrão <dcerag@br.ibm.com>
2016-11-29 13:34:14 -0200

    Python fix

--- py/seriesChild.py
+++ py/seriesChild.py
@@ -3,5 +3,5 @@ from videoParent import Video
 class Series(Video):
     """ Provides an abstraction to Serie objects - intended to be a child of Video """
 
-    def __init__(self, serie_title, serie_storyline, poster_image, youtube_url_video, episode_length, serie_storyline):
-        Video.__init__(self, serie_title, poster_image, youtube_url_video, episode_length)
+    def __init__(self, serie_title, serie_storyline, poster_image, youtube_url_video, episode_length):
+        Video.__init__(self, serie_title, poster_image, youtube_url_video, episode_length, serie_storyline)
