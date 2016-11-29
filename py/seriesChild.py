40f66b16b203cd6b01ef71f7f5f7d7a3b16c22e7
Daniel Abrão <dcerag@br.ibm.com>
2016-11-29 17:43:10 -0200

    python super test

--- py/seriesChild.py
+++ py/seriesChild.py
@@ -4,4 +4,5 @@ class Series(Video):
     """ Provides an abstraction to Serie objects - intended to be a child of Video """
 
     def __init__(self, serie_title, serie_storyline, poster_image, youtube_url_video, episode_length):
+        # super(Series, self).__init__(serie_title, poster_image, youtube_url_video, episode_length, serie_storyline)
         Video.__init__(self, serie_title, poster_image, youtube_url_video, episode_length, serie_storyline)
